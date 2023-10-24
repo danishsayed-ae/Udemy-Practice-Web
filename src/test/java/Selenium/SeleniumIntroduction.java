200524ddd6744d1b9fe51f2f5c07f8508d2cdb1b
Danish Office <danishsayed.ae@gmail.com>
2023-10-24 11:07:08 +0400

    Update compiler version to 21

--- src/test/java/Selenium/SeleniumIntroduction.java
+++ src/test/java/Selenium/SeleniumIntroduction.java
@@ -6,14 +6,15 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class SeleniumIntroduction {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		
 		//System.setProperty("WebDriver.chrome.driver", "C:\\browserdrivers\\chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
 		driver.manage().window().maximize();
-		driver.get("https://eu.astrautm.com/");
+		driver.get("https://scnsdev.astrautm.com/");
 		System.out.println(driver.getTitle());
 		System.out.println(driver.getCurrentUrl());
+		Thread.sleep(2000);
 		driver.findElement(By.id("emailid")).sendKeys("admin.astra");
 		driver.findElement(By.id("password")).sendKeys("Admin@1234**");
 		driver.findElement(By.id("btnLgnSubmit")).click();
