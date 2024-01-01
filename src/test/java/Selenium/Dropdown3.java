2cad35dce8b907e5c52e2e9a4d5c374b3df68fde
Danish <danishsayed.ae@gmail.com>
2024-01-01 19:58:30 +0400

    Added code for selecting the date.

--- src/test/java/Selenium/Dropdown3.java
+++ src/test/java/Selenium/Dropdown3.java
@@ -4,11 +4,14 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import java.time.Duration;
+
 public class Dropdown3 {
 
 	public static void main(String[] args) throws InterruptedException {
 		WebDriver driver = new ChromeDriver();
 		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
 		driver.get("https://rahulshettyacademy.com/dropdownsPractise/");
 		driver.findElement(By.id("ctl00_mainContent_ddl_originStation1_CTXT")).click();
 		//driver.findElement(By.xpath("//a[@value='PNQ']")).click();
@@ -22,7 +25,9 @@ public class Dropdown3 {
 		driver.findElement(By.xpath("//div[@id='glsctl00_mainContent_ddl_originStation1_CTNR'] //a[@value='GOI']")).click();
 		Thread.sleep(2000);
 		driver.findElement(By.xpath("//div[@id='glsctl00_mainContent_ddl_destinationStation1_CTNR'] //a[@value='JAI']")).click();
-		driver.close();
+		Thread.sleep(2000);
+		driver.findElement(By.xpath("//a[@class='ui-state-default ui-state-highlight']")).click();
+//		driver.close();
 
 		
 
