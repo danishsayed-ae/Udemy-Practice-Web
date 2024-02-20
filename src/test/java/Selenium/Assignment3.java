314bdffb46dfbf80680a5f027a426119625e401b
Danish <danishsayed.ae@gmail.com>
2024-02-20 20:15:58 +0400

    Added Assignment3.java

---
+++ src/test/java/Selenium/Assignment3.java
@@ -0,0 +1,39 @@
+package Selenium;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+import java.util.List;
+
+public class Assignment3 {
+    public static void main(String[] args) throws InterruptedException {
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://rahulshettyacademy.com/loginpagePractise/");
+        driver.findElement(By.id("username")).sendKeys("rahulshettyacademy");
+        driver.findElement(By.id("password")).sendKeys("learning");
+        driver.findElement(By.xpath("//span[contains(text(),'User')]")).click();
+//        Explicit wait
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//button[@id='okayBtn']")));
+        driver.findElement(By.xpath("//button[@id='okayBtn']")).click();
+        Select select = new Select(driver.findElement(By.xpath("//select[@class='form-control']")));
+        select.selectByValue("consult");
+        driver.findElement(By.id("terms")).click();
+        driver.findElement(By.id("signInBtn")).click();
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//a[normalize-space()='ProtoCommerce']")));
+        List<WebElement> products = driver.findElements(By.xpath("//button[@class='btn btn-info']"));
+        for(int i = 0; i < products.size(); i++)
+        {
+            driver.findElement(By.xpath("//button[@class='btn btn-info']")).click();
+        }
+        driver.findElement(By.xpath("//a[@class='nav-link btn btn-primary']")).click();
+
+    }
+}
---
+++ src/test/java/Selenium/FluentWait.java
@@ -0,0 +1,32 @@
+package Selenium;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Wait;
+
+import java.util.function.Function;
+
+import static java.util.concurrent.TimeUnit.SECONDS;
+
+public class FluentWait {
+    public static void main(String[] args, Object timeout, Object WebDriver) throws InterruptedException {
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://the-internet.herokuapp.com/dynamic_loading/1");
+        driver.findElement(By.cssSelector("div[id='start'] button")).click();
+
+        Wait wait = new FluentWait(WebDriver driver)
+                .withTimeout(timeout, SECONDS)
+                .pollingEvery(timeout, SECONDS)
+                .ignoring(Exception.class);
+
+        WebElement foo=wait.until(new Function<WebDriver, WebElement>() {
+            public WebElement applyy(WebDriver driver) {
+                return driver.findElement(By.id("foo"));
+            }
+        });
+
+    }
+}
